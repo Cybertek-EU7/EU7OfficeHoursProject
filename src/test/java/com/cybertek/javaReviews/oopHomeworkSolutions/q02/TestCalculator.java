72db0bd6df14434ffce73e25e5215310399fb026
OscarSah <oscar@cybertekschool.com>
2022-02-10 21:29:58 +0300

    OOP HW Q2

---
+++ src/test/java/com/cybertek/javaReviews/oopHomeworkSolutions/q02/TestCalculator.java
@@ -0,0 +1,9 @@
+package com.cybertek.javaReviews.oopHomeworkSolutions.q02;
+
+public class TestCalculator {
+    public static void main(String[] args) {
+        LameCalculator lc = new LameCalculator();
+
+        System.out.println(lc.plus(1,1));
+    }
+}
